123698dd2859c26e82e71a20b006a5b9ed6aef1f
DESKTOP-DDSAMEERA <digix.sameera@hotmail.com>
2021-09-29 22:23:53 +0530

    pagination number changed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::paginate(1);
+        $users = User::paginate(10);
 
         return view('users.list', compact('users'));
     }
