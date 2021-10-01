46661e4ccd2efbe67a8be337a42a7bf743621a8d
DESKTOP-DDSAMEERA <digix.sameera@hotmail.com>
2021-10-01 10:06:34 +0530

    Removed Ability of User Registeration

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
